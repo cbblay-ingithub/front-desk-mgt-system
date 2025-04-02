975d6f847a2703eef49b40a8ed1549362b38d45a
cbblay <cbblay@st.ug.edu.gh>
2025-04-02 08:00:16 +0000

    Updated Sidebar

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'vendor_name/front-desk-mgt-system',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4ff69cff6414e190fbc2b26c50100135c34765ce',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/front-desk-mgt-system' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4ff69cff6414e190fbc2b26c50100135c34765ce',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
