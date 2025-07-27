32d8d00578713739abbadf1e5c3e9f2d78af649e
cbblay <cbblay@st.ug.edu.gh>
2025-07-27 15:05:41 +0000

    included an exception

--- frontdesk_mgt/Dashboards/generate_report.php
+++ frontdesk_mgt/Dashboards/generate_report.php
@@ -16,17 +16,21 @@ if (!class_exists('\Mpdf\Mpdf')) {
 }
 
 // Create PDF instance
-$mpdf = new \Mpdf\Mpdf([
-    'mode' => 'utf-8',
-    'format' => 'A4',
-    'default_font' => 'helvetica',
-    'margin_left' => 15,
-    'margin_right' => 15,
-    'margin_top' => 25,
-    'margin_bottom' => 25,
-    'margin_header' => 10,
-    'margin_footer' => 10
-]);
+try {
+    $mpdf = new \Mpdf\Mpdf([
+        'mode' => 'utf-8',
+        'format' => 'A4',
+        'default_font' => 'helvetica',
+        'margin_left' => 15,
+        'margin_right' => 15,
+        'margin_top' => 25,
+        'margin_bottom' => 25,
+        'margin_header' => 10,
+        'margin_footer' => 10
+    ]);
+} catch (\Mpdf\MpdfException $e) {
+
+}
 
 // Get report parameters
 $reportType = $_POST['report_type'] ?? null;
