e420ddd1141c5db0b5393aa9b7575a2afcf89c5b
cbblay <cbblay@st.ug.edu.gh>
2025-07-30 13:46:29 +0000

    Designing Support staff Panel

--- frontdesk_mgt/Dashboards/help_desk.php
+++ frontdesk_mgt/Dashboards/help_desk.php
@@ -85,7 +85,6 @@ $conn->close();
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="help_desk.css">
-    <link rel="stylesheet" href="sidebar.css">
     <link rel="stylesheet" href="notification.css">
     <style>
         /* Dropdown menu styles */
